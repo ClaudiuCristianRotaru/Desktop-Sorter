b22764ae2bf00411bbc53567205e79334031df66
Claudiu <clayarname4@gmail.com>
2024-05-28 21:57:19 +0300

    Serialize SortedFolder to json method

--- classes/SortedFolder.py
+++ classes/SortedFolder.py
@@ -1,4 +1,13 @@
+from dataclasses import dataclass, asdict
+import json
+
+@dataclass
 class SortedFolder:
+    name: int
+    associated_types: list[str]
     def __init__(self, name: str, associated_types: list[str]):
         self.name: str = name
         self.associated_types: list[str] = associated_types
+
+    def to_json(self):
+        return json.dumps(asdict(self), indent=4)
