386e5e80e262772c0605d0ba2735e605fad282d7
Claudiu <clayarname4@gmail.com>
2024-06-05 14:43:31 +0300

    Create constructor

--- classes/Config.py
+++ classes/Config.py
@@ -4,3 +4,7 @@ class Config:
     excluded_files: list[str]
     sorting_folders: list[SortedFolder]
     
+    def __init__(self, default_folder: str, excluded_files: list[str], sorting_folders: list[SortedFolder]):
+        self.default_folder: str = default_folder
+        self.excluded_files: list[str] = excluded_files
+        self.sorting_folders: list[SortedFolder] = sorting_folders
