ad8afb4e89cc0c496d6ea9d361af6a74066ecca1
Claudiu <clayarname4@gmail.com>
2024-06-05 16:08:30 +0300

    Replace use of folder list with config class

--- classes/ConfigManager.py
+++ classes/ConfigManager.py
@@ -1,4 +1,5 @@
 from classes.SortedFolder import SortedFolder
+from classes.Config import Config
 import json
 
 
@@ -7,28 +8,23 @@ class ConfigManager:
         with open(path, "r") as file:
             return file.read()
 
-    def deserialize_config_json(json_data_string: str) -> list[SortedFolder]:
+    def deserialize_config_json(json_data_string: str) -> Config:
         json_data_dict = json.loads(json_data_string)
+
+        default_folder = json_data_dict["default_folder"]
+        excluded_files: list[str] = json_data_dict["excluded_files"]
+        
+        sorting_folders_dict = json_data_dict["sorting_folders"]
         sorted_folders: list[SortedFolder] = []
-        for folder in json_data_dict:
-            dir: SortedFolder = SortedFolder("", [])
-
-            dir.name = folder["name"]
-
+        for folder in sorting_folders_dict:
+            name = folder["name"]
             associated_types = folder["associated_types"]
-            for type in associated_types:
-                dir.associated_types.append(type)
+            sorted_folders.append(SortedFolder(name, associated_types))
 
-            sorted_folders.append(dir)
-        return sorted_folders
+        print(type(associated_types[0]))
+        config: Config = Config(default_folder, excluded_files, sorted_folders)
+        return config
 
-    def save_config_to_file(path: str, sorted_folders: list[SortedFolder]) -> None:
+    def save_config_to_file(path: str, config: Config) -> None:
         with open(path, "w") as file:
-            file.write("[")
-            i: int
-            for i in range(len(sorted_folders) - 1):
-                file.write(sorted_folders[i].to_json())
-                file.write(",\n")
-            if len(sorted_folders) > 1:
-                file.write(sorted_folders[i + 1].to_json())
-            file.write("]")
+            file.write(json.dumps(config.__dict__, indent=4))
