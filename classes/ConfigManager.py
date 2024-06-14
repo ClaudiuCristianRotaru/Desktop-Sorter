00a21f0fd927868f55a38fe196aac378afe024ec
Claudiu <clayarname4@gmail.com>
2024-06-14 23:57:09 +0300

    Create encoder for json serialization

--- classes/ConfigManager.py
+++ classes/ConfigManager.py
@@ -11,6 +11,7 @@ class ConfigManager:
     def deserialize_config_json(json_data_string: str) -> Config:
         json_data_dict = json.loads(json_data_string)
 
+        print(type(json_data_dict))
         default_folder = json_data_dict["default_folder"]
         excluded_files: list[str] = json_data_dict["excluded_files"]
         
@@ -25,6 +26,13 @@ class ConfigManager:
         config: Config = Config(default_folder, excluded_files, sorted_folders)
         return config
 
-    def save_config_to_file(path: str, config: Config) -> None:
+    def complex_encoder(obj):
+        if hasattr(obj, 'jsonable'):
+            return obj.jsonable()
+        else:
+            raise TypeError(f'Object of type {type(obj)} with value of {repr(obj)} is not JSON serializable')
+
+
+    def save_config_to_file(self, path: str, config: Config) -> None:
         with open(path, "w") as file:
-            file.write(json.dumps(config.__dict__, indent=4))
+            return file.write(json.dumps(config.__dict__, indent=4, default= self.complex_encoder))
